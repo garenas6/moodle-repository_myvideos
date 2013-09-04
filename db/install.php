9eb8279a8de33ffba07c0c6ffdb22ec9449304b6
gloria <gloria.arenas@estudiants.urv.cat>
2013-09-04 12:08:06 +0200

    Soft bug. Changing data base manager

--- db/install.php
+++ db/install.php
@@ -3,8 +3,9 @@
 function xmldb_repository_myvideos_install() {
     global $CFG, $DB;
     $result = true;
+    $dbman = $DB->get_manager();
 
-    if (!$DB->table_exists('myvideos_video')) {
+    if (!$dbman->table_exists('myvideos_video')) {
         debugging('myvideos repository does not work if block_myvideos database tables are not present');
         return false;
     }
