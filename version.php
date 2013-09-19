3960a9e77fb86d6b928ff2aeb13cc0ba80649a0b
David Monllao <davidm@moodle.com>
2013-09-19 13:07:14 +0800

    Bump version

--- version.php
+++ version.php
@@ -11,6 +11,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2013073100;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2013091900;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2012112900;        // Requires this Moodle version
 $plugin->component = 'repository_myvideos'; // Full name of the plugin (used for diagnostics)
